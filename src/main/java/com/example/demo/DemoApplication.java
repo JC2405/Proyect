56969b87cf66a9d11dc215837da18c199009b0e6
JC2405 <chaparrobarrerajulian@gmail.com>
2025-03-18 13:51:40 -0500

    imprimir

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -14,6 +16,9 @@ public class DemoApplication {
 			"Gabriel",
 
 		};
+		for (String nombre : nombres) {
+			JOptionPane.showMessageDialog(null, nombre);
+		}
 
 	}
 
